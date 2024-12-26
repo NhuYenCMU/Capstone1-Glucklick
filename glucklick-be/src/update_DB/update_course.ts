12776025ba1bed9cbf7fca554d667f8234e12e4c
VoNhuYen <vothinhuyen22042003@gmail.com>
2024-12-26 12:11:16 +0700

    tam

--- glucklick-be/src/update_DB/update_course.ts
+++ glucklick-be/src/update_DB/update_course.ts
@@ -10,45 +10,29 @@ const addSampleCourses = async () => {
     // Thêm các khóa học mẫu vào MongoDB
     const courses = await Course.insertMany([
       {
-        courseName: 'Python cho người mới bắt đầu',
-        description: 'Khóa học cung cấp trọn bộ kiến thức cơ bản của lập trình Python, học viên có thể tạo ra một ứng dụng hoàn chỉnh với Python sau khi hoàn thành khóa học.',
+        courseName: 'Lập trình Java Backend Full Steps',
+        description: 'Viết tài liệu yêu cầu, thiết kế infra, thiết kế database, thiết kế UI/UX, Phát triển Ứng dụng, Triển khai CI/CD.',
         difficultyLevel: 'Beginner',
-        price: 499, // Giá tiền mẫu
-        rating: 4.9, // Đánh giá mẫu
-        link: 'https://codelearn.io/learning/python-cho-nguoi-moi-bat-dau'
+        price: 299, // Giá tiền mẫu
+        rating: 4.6, // Đánh giá mẫu
+        link: 'https://www.udemy.com/course/lap-trinh-java-backend-full-steps/?couponCode=2021PM25'
       },
       {
-        courseName: 'Giải thuật cho Python',
-        description: 'Khóa học này sẽ cung cấp kiến thức về thiết kế, phân tích và triển khai thuật toán bằng Python. Học viên sẽ trang bị các kỹ thuật thuật toán cơ bản, phân tích độ phức tạp, và giải quyết các vấn đề tính toán.',
-        difficultyLevel: 'Intermediate',
-        price: 699,
-        rating: 4.9,
-        link: 'https://codelearn.io/learning/giai-thuat-cho-python'
+        courseName: 'Lập trình Java cho người mới học',
+        description: 'Lý thuyết ngắn gọn, thực hành chi tiết dễ hiểu và vận dụng viết mã dự án',
+        difficultyLevel: 'Beginner',
+        price: 349,
+        rating: 4.8,
+        link: 'https://www.udemy.com/course/lap-trinh-java-cho-nguoi-moi-hoc/?couponCode=2021PM25'
       },
       {
-        courseName: 'Lập trình C++ cơ bản',
-        description: 'Khóa học cung cấp nền tảng vững chắc về C++ cho người mới bắt đầu, giúp họ phát triển kỹ năng lập trình cơ bản và áp dụng vào các dự án thực tế.',
+        courseName: 'Build Basic Android Apps with Java',
+        description: 'By the end of this Skill Path, you will have created your very own fully functional quiz game for Android Devices with Java. Includes Java, XML, Android Studio, Wireframing, Layout Editor, Material Design, and more..',
         difficultyLevel: 'Beginner',
         price: 499,
         rating: 4.8,
-        link: 'https://codelearn.io/learning/lap-trinh-cpp-co-ban'
+        link: 'https://www.codecademy.com/learn/paths/introduction-to-android-with-java'
       },
-      {
-        courseName: 'Lập trình C++ nâng cao',
-        description: 'Khóa học C++ nâng cao được thiết kế dành cho những lập trình viên đã có kiến thức cơ bản về C++ và mong muốn nâng cao kỹ năng lập trình của mình.',
-        difficultyLevel: 'Advanced',
-        price: 180,
-        rating: 4.6,
-        link: 'https://codelearn.io/learning/lap-trinh-cpp-nang-cao'
-      },
-      {
-        courseName: 'Lập trình hướng đối tượng trong C++',
-        description: 'Khóa học giúp các lập trình viên học được kỹ thuật lập trình mà tất cả logic, yêu cầu thực tế đều được xây dựng xoay quanh đối tượng',
-        difficultyLevel: 'Beginner',
-        price: 0,
-        rating: 4.9,
-        link: 'https://codelearn.io/learning/lap-trinh-huong-doi-tuong-trong-cpp'
-      }
     ])
 
     console.log('Đã thêm dữ liệu mẫu cho khóa học:', courses)
